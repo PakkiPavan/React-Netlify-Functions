b3b63342e9672df36da52502dc228d52d2f6dce1
Pakki Pavan <pavanpakki786@gmail.com>
2021-08-24 17:34:23 +0530

    changes

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,9 +1,9 @@
 
-const puppeteer = require("puppeteer-extra");
-//const fs = require("fs");
-//const path = require("path");
+//const puppeteer = require("puppeteer-extra");
+const fs = require("fs");
+const path = require("path");
 //const pluginStealth = require("puppeteer-extra-plugin-stealth");
-//const chalk = require("chalk");
+const chalk = require("chalk");
 
 
 exports.handler = async(event) => {
