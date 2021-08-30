2f398b902a52222ce8d3a47c860c1db07229be70
PakkiPavan <32393106+PakkiPavan@users.noreply.github.com>
2021-08-30 17:24:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,10 @@ function App() {
 		.then(data=>{
 			console.log("SUCCESS");
 			console.log(data);
-                        /*fetch('.netlify/functions/login',{
+                        fetch('.netlify/functions/login',{
                          method:'POST',
                          body: JSON.stringify(data)
-                        })*/
+                        })
                         
                         
 		})
