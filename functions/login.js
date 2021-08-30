00b964fea29b0012dd400f8630d67266f4f93d3c
PakkiPavan <32393106+PakkiPavan@users.noreply.github.com>
2021-08-30 17:37:21 +0530

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,16 +1,22 @@
 //const flatted=require("flatted");
 //const chromium = require('chrome-aws-lambda');
 //const fs = require("fs");
-
+console.log("OUTSIDE LOGIN");
+console.log(global)
+console.log(global.test1);
+console.log(global.hello);
 exports.handler=async(event)=>{
-  console.log("LOGIN FUNCTION");
+  console.log("LOGIN INSIDE FUNCTION");
+console.log(global)
+console.log(global.test1);
+console.log(global.hello);
   let page=JSON.parse(event.body);
   /*let waitForSelectorFun=page.waitForSelector;
   waitForSelectorFun=waitForSelectorFun.replace(/\n/g,"");
   waitForSelectorFun="function "+waitForSelectorFun;
   eval(waitForSelectorFun);
   await waitForSelector(`input[type='password']`);*/
-  console.log(global.page);
+  //console.log(global.page);
   console.log("LOGGED IN");
   let msg={
    loggedIn:true
