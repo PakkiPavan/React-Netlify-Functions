d5f8cd58e8065c96062944d9bcbc6b15cfcae5df
PakkiPavan <32393106+PakkiPavan@users.noreply.github.com>
2021-08-30 16:33:58 +0530

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -5,11 +5,11 @@
 exports.handler=async(event)=>{
   console.log("LOGIN FUNCTION");
   let page=JSON.parse(event.body);
-  let waitForSelectorFun=page.waitForSelector;
+  /*let waitForSelectorFun=page.waitForSelector;
   waitForSelectorFun=waitForSelectorFun.replace(/\n/g,"");
   waitForSelectorFun="function "+waitForSelectorFun;
   eval(waitForSelectorFun);
-  await waitForSelector(`input[type='password']`);
+  await waitForSelector(`input[type='password']`);*/
   
   console.log("LOGGED IN");
   let msg={
